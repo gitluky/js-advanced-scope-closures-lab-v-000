ff502eaef44bee815425aa2bf25c3bf9681e9e85
Jonathan Luk <jluk87@gmail.com>
2019-08-13 18:13:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function produceDrivingRange (blocks) {
+  return function (firstBlock, secondBlock) {
+    const numberOfBlocks = Math.abs(parseInt(firstBlock.slice(0, -2)) - parseInt(secondBlock.slice(0, -2)));
+    if (numberOfBlocks > blocks) {
+      return `${numberOfBlocks - blocks} blocks out of range`
+    } else {
+      return `within range by ${blocks - numberOfBlocks}`
+    }
+  }
+}
+
+function produceTipCalculator (percentage) {
+  return function (bill) {
+    return bill * percentage;
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+  return class {
+    constructor (name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
