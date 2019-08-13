92bc4528191e41238e98c6537ef0603aad2c5c3a
Jonathan Luk <jluk87@gmail.com>
2019-08-13 18:02:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange (blocks) {
   return function (firstBlock, secondBlock) {
-    const numberOfBlocks = Math.abs(parseInt(firstBlock.slice(0, -2)) - parseInt(firstBlock.slice(0, -2)));
+    const numberOfBlocks = Math.abs(parseInt(firstBlock.slice(0, -2)) - parseInt(secondBlock.slice(0, -2)));
     debugger;
     if (numberOfBlocks > blocks) {
       return `${numberOfBlocks - blocks} blocks out of range`
